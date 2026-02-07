8174c390da46cd1e16930bc61328c43fd5def4d8
AguirreSann <felipe.sa1986@gmail.com>
2026-02-07 18:32:16 -0300

    first commit

---
+++ apps/courses/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def course_list(request):
+    courses =[
+        {
+            'id': 1,
+            'level': 'Principiante',
+            'rating': 4.8,
+            'course_title': 'Python: Fundamentos hasta los detalles',
+            'instructor': 'Alison Walsh',
+            'course_image': 'images/curso_1.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/women/68.jpg'
+        },
+        {
+            'id': 2,
+            'level': 'Principiante',
+            'rating': 5.0,
+            'course_title': 'Django: Aplicaciones robustas',
+            'instructor': 'Patty Kutch',
+            'course_image': 'images/curso_2.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/women/20.jpg'
+        },
+        {
+            'id': 3,
+            'level': 'Principiante',
+            'rating': 5.0,
+            'course_title': 'Fast API',
+            'instructor': 'Alonzo Murray',
+            'course_image': 'images/curso_3.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/men/32.jpg'
+        },
+        {
+            'id': 4,
+            'level': 'Principiante',
+            'rating': 5.0,
+            'course_title': 'Django Resst',
+            'instructor': 'Gregory Harris',
+            'course_image': 'images/curso_4.jpg',
+            'instructor_image': 'https://randomuser.me/api/portraits/men/45.jpg'
+        }
+    ]
+    return render(request, 'courses\courses.html', {
+        'courses': courses
+    })
+
+def course_detail(request):
+    course = {
+        'course_title': 'Django Aplicaciones',
+        'course_link': '',
+        'info_course': {
+            'lessons': 79,
+            'duration': 8,
+            'instructor': 'Ricardo Cuellar'
+        },
+        'course_content':
+            {
+                'id': 1,
+                'name': 'Introducción al curso',
+                'lessons': [
+                    {
+                        'name': 'Qué aprenderas en el curso?',
+                        'type': 'video'
+                    },
+                    {
+                        'name': 'Como usar la plataforma?',
+                        'type': 'articule'
+                    }
+                ]
+            }
+    }
+    return render(request, 'courses/course_detail.html', {
+        'course': course
+    })
+
+def course_lessons(request):
+    pass
