87e623a174eba2868dab62973ead041df8e27901
AguirreSann <felipe.sa1986@gmail.com>
2026-02-14 00:08:57 -0300

    todas las vistas estan funcionando

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -48,7 +48,8 @@ def course_list(request):
 def course_detail(request):
     course = {
         'course_title': 'Django Aplicaciones',
-        'course_link': '',
+        'course_link': 'course_lessons',
+        'course_image': 'images/curso_2.jpg',
         'info_course': {
             'lessons': 79,
             'duration': 8,
@@ -76,4 +77,44 @@ def course_detail(request):
     })
 
 def course_lessons(request):
-    pass
+    lessons = {
+        'course_title': 'Django Aplicaciones',
+        'course_progress': 30,
+        'course_content': [
+            {
+                'id': 1,
+                'name': 'Introducción al curso',
+                'total_lessons': 6,
+                'complete_lessons': 3,
+                'lessons': [
+                    {
+                        'name': 'Qué aprenderas en el curso?',
+                        'type': 'video'
+                    },
+                    {
+                        'name': 'Como usar la plataforma?',
+                        'type': 'articule'
+                    }
+                ]
+            },
+            {
+                'id': 2,
+                'name': 'Django principios',
+                'total_lessons': 12,
+                'complete_lessons': 2,
+                'lessons': [
+                    {
+                        'name': 'Qué aprenderas en el curso?',
+                        'type': 'video'
+                    },
+                    {
+                        'name': 'Como usar la plataforma?',
+                        'type': 'articule'
+                    }
+                ]
+            }
+        ]
+    }
+    return render(request, 'courses/course_lessons.html',{
+        'lesson': lessons
+    })
